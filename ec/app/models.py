cb5df67e913dd437212a6b87b0d63ea00ce7703b
Pratyush Jha <pratyush.jha15@gmail.com>
2023-05-26 20:14:53 +0530

    First Commit

--- ec/app/models.py
+++ ec/app/models.py
@@ -50,7 +50,7 @@ class Product(models.Model):
     prodapp=models.TextField(default='')  
     category=models.CharField(choices=CATEGORY_CHOICES, max_length=2)    
     product_image=models.ImageField(upload_to='category')             
-    def __str__(self): 
+    def __str__(self):  
         return self.title
     
 class Customer(models.Model): 
@@ -75,13 +75,12 @@ class Cart(models.Model):
         return self.quantity * self.product.discounted_price  
     
 class OrderPlaced(models.Model):   
-    user=models.ForeignKey(User,on_delete=models.CASCADE)  
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)   
+    user=models.ForeignKey(User,on_delete=models.CASCADE)    
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)    
     quantity=models.PositiveIntegerField(default=1)    
-    is_cancelled=models.BooleanField(default=False)         
-
+    is_cancelled=models.BooleanField(default=False)           
     @property 
-    def total_cost(self): 
+    def total_cost(self):   
         return self.quantity * self.product.discounted_price   
 
 class Wishlist(models.Model): 
